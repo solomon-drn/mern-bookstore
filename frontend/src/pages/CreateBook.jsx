f08c51ccfccde33fbe604935c2426ea4718fa440
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-21 11:18:10 +0000

    feat: add edit book page

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -12,7 +12,7 @@ const CreateBook = () => {
   const navigate = useNavigate();
 
   const saveBook = () => {
-    setLoading(true)
+    setLoading(true);
     const data = { title, author, publishYear };
     axios
       .post("http://localhost:4000/api/v1/books/create", data)
@@ -34,40 +34,36 @@ const CreateBook = () => {
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">
-            Title
-          </label>
-          <input type="text"
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">
-            Author
-          </label>
-          <input type="text"
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">
-            Publish Year
-          </label>
-          <input type="text"
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="text"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
-          <div className="flex justify-center">
-
-          <button className="p-2 w-100 bg-sky-300 m-8 w-30" onClick={saveBook}>
+        </div>
+        <div className="flex justify-center">
+          <button className="p-2 w-full bg-sky-300 m-8 " onClick={saveBook}>
             Save
           </button>
-          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
-const Editbook = () => {
+const EditBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/books/${id}`)
+      .then((response) => {
+        setTitle(response.data.title);
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error occurred. Please check your console");
+        console.log(error)
+      });
+  }, [id]);
+
+  const editBook = () => {
+    setLoading(true);
+    const data = { title, author, publishYear };
+    axios
+      .patch(`http://localhost:4000/api/v1/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error occurred. Please check the console");
+        console.log(error);
+      });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Edit Book</h1>
+      {loading ? <Spinner /> : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+          <div className="my-4">
+            <label className="text-xl mr-4 text-gray-500">Title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-gray-500">Author</label>
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+            <input
+              type="text"
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="p-2 w-full bg-sky-300 m-8"
+              onClick={editBook}
+            >
+              Save
+            </button>
+          </div>
+        </div>)}
+    </div >
+  );
+};
 
-export default Editbook
+export default EditBook;
