b9ea096597e091763ef7333fa4c8de044547601b
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-26 07:19:24 +0000

    feat: protect book creation route

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { authenticateUser } from "../middleware/auth.middleware.js";
 import { createBook, deleteBook, getAllBooks, getBook, updateBook } from "../controllers/book.controller.js";
 
 const router = express.Router();
 
-router.post("/create", createBook);
+router.post("/create", authenticateUser, createBook);
 router.get("/", getAllBooks)
 router.get("/:id", getBook)
 router.patch("/:id", updateBook)
