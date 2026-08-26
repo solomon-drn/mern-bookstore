b919bb783649b0b04fe458781e3fad8a07eef613
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-26 07:33:51 +0000

    feat: protect book update and delete routes

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { authenticateUser } from "../middleware/auth.middleware.js";
-import { createBook, deleteBook, getAllBooks, getBook, updateBook } from "../controllers/book.controller.js";
+import {
+  createBook,
+  deleteBook,
+  getAllBooks,
+  getBook,
+  updateBook,
+} from "../controllers/book.controller.js";
 
 const router = express.Router();
 
 router.post("/create", authenticateUser, createBook);
-router.get("/", getAllBooks)
-router.get("/:id", getBook)
-router.patch("/:id", updateBook)
-router.delete("/:id", deleteBook)
+router.get("/", getAllBooks);
+router.get("/:id", getBook);
+router.patch("/:id", authenticateUser, updateBook);
+router.delete("/:id", authenticateUser, deleteBook);
 
 export default router;
