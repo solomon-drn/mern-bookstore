031ffd6af6c9339aa29614b25290577b1a13718f
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-30 06:38:15 +0000

    feat: add axios auth interceptor

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
-import axios from "axios";
+import api from "../api";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Toast from "../components/Toast";
@@ -18,8 +18,8 @@ const CreateBook = () => {
     setLoading(true);
     setToast(null);
     const data = { title, author, publishYear };
-    axios
-      .post("http://localhost:4000/api/v1/books/create", data)
+    api
+      .post("/books/create", data)
       .then(() => {
         setLoading(false);
         navigate("/", {
@@ -49,27 +49,38 @@ const CreateBook = () => {
       {toast && <Toast message={toast.message} type={toast.type} />}
       <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <label htmlFor="title" className="text-xl mr-4 text-gray-500">
+            Title
+          </label>
           <input
+            id="title"
             type="text"
+            autoComplete="off"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <label htmlFor="author" className="text-xl mr-4 text-gray-500">
+            Author
+          </label>
           <input
+            id="author"
             type="text"
+            autoComplete="off"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
         </div>
         <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <label htmlFor="publishYear" className="text-xl mr-4 text-gray-500">
+            Publish Year
+          </label>
           <input
-            type="text"
+            id="publishYear"
+            type="number"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
