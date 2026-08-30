99a237cd3006c6aff6462b37159d9a72cac72a06
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-30 07:12:37 +0000

    refactor: migrate EditBook API requests to api instance

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
-import axios from "axios";
+import api from "../api";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "../components/Button";
 
@@ -16,8 +16,8 @@ const EditBook = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:4000/api/v1/books/${id}`)
+    api
+      .get(`/books/${id}`)
       .then((response) => {
         setTitle(response.data.title);
         setAuthor(response.data.author);
@@ -37,8 +37,8 @@ const EditBook = () => {
 
     const data = { title, author, publishYear };
 
-    axios
-      .patch(`http://localhost:4000/api/v1/books/${id}`, data)
+    api
+      .patch(`/books/${id}`, data)
       .then(() => {
         setSaving(false);
         navigate("/", {
@@ -68,27 +68,38 @@ const EditBook = () => {
       ) : (
         <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
           <div className="my-4">
-            <label className="text-xl mr-4 text-gray-500">Title</label>
+            <label htmlFor="title" className="text-xl mr-4 text-gray-500">
+              Title
+            </label>
             <input
+              id="title"
               type="text"
+              autoComplete="off"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               className="border-2 border-gray-500 px-4 py-2 w-full"
             />
           </div>
           <div className="my-4">
-            <label className="text-xl mr-4 text-gray-500">Author</label>
+            <label htmlFor="author" className="text-xl mr-4 text-gray-500">
+              Author
+            </label>
             <input
+              id="author"
               type="text"
+              autoComplete="off"
               value={author}
               onChange={(e) => setAuthor(e.target.value)}
               className="border-2 border-gray-500 px-4 py-2 w-full"
             />
           </div>
           <div className="my-4">
-            <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+            <label htmlFor="publishYear" className="text-xl mr-4 text-gray-500">
+              Publish Year
+            </label>
             <input
-              type="text"
+              id="publishYear"
+              type="number"
               value={publishYear}
               onChange={(e) => setPublishYear(e.target.value)}
               className="border-2 border-gray-500 px-4 py-2 w-full"
