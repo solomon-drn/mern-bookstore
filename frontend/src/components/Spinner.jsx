ed326a7d56c414052fe7431ee797b0dcde237082
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-30 07:20:45 +0000

    style: center loading spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-
 const Spinner = () => {
   return (
-    <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
+    <div className="flex justify-center items-center min-h-[70vh] w-full">
+      <div className="animate-ping w-16 h-16 rounded-full bg-sky-600"></div>
+    </div>
   );
 };
 
