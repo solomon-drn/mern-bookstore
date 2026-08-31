2b972e245d4ff8259e12e40e8512f6fd8864daa1
solomon-drn <sdgmasterpiece@gmail.com>
2026-08-31 06:14:04 +0000

    refactor: use environment variable for API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -12,6 +12,6 @@ api.interceptors.request.use((config) => {
   }
 
   return config;
-})
+});
 
 export default api;
